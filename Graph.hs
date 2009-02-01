c8a63a31e891beeac1c73de184bfbd336ac35a3b
Vo Minh Thu <autumnae@gmail.com>
2009-02-01 23:50:04 +0100

    NodeType replaced with TypeRep; RFloat and RInt replaced by TRef Float and TRef Int; does the same thing with less amount of code.

--- Graph.hs
+++ Graph.hs
@@ -1,20 +1,25 @@
-{-# LANGUAGE ViewPatterns, FlexibleInstances #-}
+{-# LANGUAGE ViewPatterns, FlexibleInstances, TypeSynonymInstances #-}
 -- 2009.01.08
--- 2009.01.27
+-- 2009.02.02
 -- autumnae
 --
 -- The graph is made of nodes named/referenced by Int's.
 -- Nodes have a field denoting their type, but otherwise
 -- have all the same type (i.e. Node).
+--
 -- To gain some safety from Haskell's type system,
--- function to create and combine nodes are typed.
+-- function to create and combine nodes are typed :
+-- nodes have the same type (so they can be put inside
+-- a Map) but their reference are typed.
+--
 -- But nothing enforces the relationship between the type
 -- of the typed references and the value denoting that type
 -- inside the Node. See for instance how the Float type
 -- appearing in the type signature of baseInt is related
--- to the Float value (of type NodeType) in the constructed
+-- to the Float value (of type TypeRep) in the constructed
 -- node. Maybe I could use TH to generate some of this stuff.
 --
+-- 'usage' : in ghci : doDot ex1
 --
 module Graph where
 
@@ -22,43 +27,38 @@ import Control.Monad.State
 import qualified Data.IntMap as IM
 import Data.List
 import Data.Typeable
+import Data.Dynamic
 
 true = True
 false = False
 
--- Possible type value for a node. It should match the
--- type of the reference.
-data NodeType = Int | Float
-  deriving Show
+int = typeOf (undefined :: Int)
+float = typeOf (undefined :: Float)
+
+myTypeOf t = go (typeOf t) []
+  where
+    go t acc =
+      if null xs
+        then (reverse acc,t)
+        else go (last xs) (head xs : acc)
+      where xs = typeRepArgs t
 
 -- A reference is simply an Int. But to gain some type-safety
 -- the reference are wrapped (below).
 type Ref = Int
 
--- A class is provided to get the untypeded reference and
--- wrap it. Also, to give the value representing the type.
-class WrapRef w where
-  unwrapRef :: w -> Ref
-  wrapRef :: Ref -> w
+-- A typed reference.
+data TRef a = TRef Ref
 
-data RInt = RInt Ref
-
-instance WrapRef RInt where
-  unwrapRef (RInt n) = n
-  wrapRef n = RInt n
-
-data RFloat = RFloat Ref
-
-instance WrapRef RFloat where
-  unwrapRef (RFloat n) = n
-  wrapRef n = RFloat n
+type RInt = TRef Int
+type RFloat = TRef Float
 
 type Rank = Int
 
 -- e.g. Op Int "plus" [a,b] [c] means the node Plus depends on a and b (the parents)
 -- and c depends on it (c is a child of Plus).
-data Node = Base NodeType String [Ref]
-          | Op NodeType String  [Ref] [Ref] Rank -- parents, children
+data Node = Base TypeRep String [Ref]
+          | Op TypeRep String  [Ref] [Ref] Rank -- parents, children
   deriving Show
 
 info (Base _ i _) = i
@@ -77,9 +77,11 @@ children (Op _ _ _ c _) = c
 data G = G { nodes :: IM.IntMap Node, nextName :: Ref }
   deriving Show
 
+type N a = State G (TRef a)
+
 emptyGraph = G IM.empty 0
 
-graph :: State G a -> IM.IntMap Node
+graph :: N a -> IM.IntMap Node
 graph = nodes . (flip execState emptyGraph)
 
 alist = IM.toList . graph
@@ -105,33 +107,30 @@ doDot g = do
   putStr $ dot2 g
   putStrLn "}"
 
-instance WrapRef w => Show (State G w) where
---  show g = show (ns IM.! i)
---    where (unwrapRef -> i,nodes -> ns) = runState g emptyGraph
+instance Show (N w) where
   show g = show $ execState g emptyGraph
 
-instance WrapRef w => Eq (State G w) where
+instance Eq (N w) where
   a == b = error "TODO: Eq instance for WrapRef."
 
-instance Num (State G RInt) where
-  (+) = nAdd
-  (*) = nMul
-  signum = nSignum
-  abs = nAbs
+instance Num (N Int) where
+  (+) = lift2 (+) "add"
+  (*) = lift2 (*) "mul"
+  signum = lift1 signum "signumf"
+  abs = lift1 abs "abs"
   fromInteger = baseInt . show
 
-instance Num (State G RFloat) where
-  (+) = nAddF
-  (*) = nMulF
-  signum = nSignumF
-  abs = nAbsF
+instance Num (N Float) where
+  (+) = lift2 (+) "add"
+  (*) = lift2 (*) "mul"
+  signum = lift1 signum "signum"
+  abs = lift1 abs "absf"
   fromInteger =  baseFloat . show . fromInteger
 
-instance Fractional (State G RFloat) where
-  (/) = nDivF
-  recip = nRecipF
+instance Fractional (N Float) where
+  (/) = lift2 (/) "divf"
+  recip = lift1 recip "recip"
   fromRational = baseFloat . show . fromRational
-
 
 addChild' c (Base t info cs) = Base t info (c:cs)
 addChild' c (Op t info ps cs rk) = Op t info ps (c:cs) rk
@@ -154,78 +153,37 @@ addChild c n = do
   g <- get
   put $ g { nodes = IM.adjust (addChild' c) n ns }
 
-baseInt :: String -> State G RInt
-baseInt info = mkNode (Base Int info []) >>= return . wrapRef
+base :: Typeable a => a -> String -> N a
+base v info = mkNode (Base (typeOf v) info []) >>= return . TRef
 
-baseFloat :: String -> State G RFloat
-baseFloat info = mkNode (Base Float info []) >>= return . wrapRef
+baseInt = base (undefined::Int)
 
-unOp :: String -> State G RInt -> State G RInt
-unOp name a = do
-  (unwrapRef -> n1) <- a
+baseFloat = base (undefined::Float)
+
+lift1 :: (Typeable a, Typeable b) => (a -> b) -> String -> (N a -> N b)
+lift1 f name = \a -> do
+  TRef n1 <- a
   rk <- gets (rank . (IM.! n1) . nodes)
   n <- gets nextName
   addChild n n1
-  addNode n (Op Int name [n1] [] (rk + 1))
-  return (wrapRef n)
+  addNode n (Op (typeOf $ f undefined) name [n1] [] (rk + 1))
+  return (TRef n)
 
-binOp :: String -> State G RInt -> State G RInt -> State G RInt
-binOp name a b = do
-  (unwrapRef -> n1) <- a
-  (unwrapRef -> n2) <- b
+lift2 :: (Typeable a, Typeable b, Typeable c) => (a -> b -> c) -> String -> (N a -> N b -> N c)
+lift2 f name = \a b -> do
+  TRef n1 <- a
+  TRef n2 <- b
   rk1 <- gets (rank . (IM.! n1) . nodes)
   rk2 <- gets (rank . (IM.! n2) . nodes)
   n <- gets nextName
   addChild n n1
   addChild n n2
-  addNode n (Op Int name [n1,n2] [] (max rk1 rk2 + 1))
-  return (wrapRef n)
+  addNode n (Op (typeOf $ f undefined) name [n1,n2] [] (max rk1 rk2 + 1))
+  return (TRef n)
 
-unOpF :: String -> State G RFloat -> State G RFloat
-unOpF name a = do
-  (unwrapRef -> n1) <- a
-  rk <- gets (rank . (IM.! n1) . nodes)
-  n <- gets nextName
-  addChild n n1
-  addNode n (Op Float name [n1] [] (rk + 1))
-  return (wrapRef n)
-
-binOpF :: String -> State G RFloat -> State G RFloat -> State G RFloat
-binOpF name a b = do
-  (unwrapRef -> n1) <- a
-  (unwrapRef -> n2) <- b
-  rk1 <- gets (rank . (IM.! n1) . nodes)
-  rk2 <- gets (rank . (IM.! n2) . nodes)
-  n <- gets nextName
-  addChild n n1
-  addChild n n2
-  addNode n (Op Float name [n1,n2] [] (max rk1 rk2 + 1))
-  return (wrapRef n)
-
-nAdd, nMul :: State G RInt -> State G RInt -> State G RInt
-nAdd = binOp "add"
-nMul = binOp "mul"
-nAbs = unOp "abs"
-nSignum = unOp "signum"
-
-nAddF = binOpF "addf"
-nMulF = binOpF "mulf"
-nAbsF = unOpF "absf"
-nSignumF = unOpF "signumf"
-nDivF = binOpF "divf"
-nRecipF = unOpF "recipf"
-
-foo :: State G RInt -> State G RFloat -> State G RFloat
-foo x s = do
-  (unwrapRef -> n1) <- x
-  (unwrapRef -> n2) <- s
-  rk1 <- gets (rank . (IM.! n1) . nodes)
-  rk2 <- gets (rank . (IM.! n2) . nodes)
-  n <- gets nextName
-  addChild n n1
-  addChild n n2
-  addNode n (Op Float "foo" [n1,n2] [] (max rk1 rk2 + 1))
-  return (wrapRef n)
+foo = lift2 f "foo"
+  where f :: Int -> Float -> Float
+        f x s = 1.0
 
 milliseconds = baseInt "milliseconds"
 
